13e144407af478b227f698159d246d3b870afd6b
mogongzi <superrenyuan@hotmail.com>
2022-05-19 17:10:21 +0800

    add insertion sort image

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,17 +1,14 @@
 def insertion_sort(array)
-  (0..array.size - 1).each_with_index do |index|
-    temp = array[index]
-    position = index - 1
-
+  (1..array.size - 1).each do |i|
+    temp = array[i]
+    position = i - 1
     while position >= 0
-      if array[position] > temp
+      if array[position] > array[position + 1]
         array[position + 1] = array[position]
-        position -= 1
-      else
-        break
+        array[position] = temp
       end
+      position -= 1
     end
-    array[position + 1] = temp
   end
 end
 
@@ -19,8 +16,8 @@ require 'test/unit'
 
 class InsertionSort < Test::Unit::TestCase
   def test_insertion_sort
-    array = [4, 2, 7, 3, 1, 8, 9]
+    array = [4, 2, 5, 8, 9, 7, 1, 3]
     insertion_sort(array)
-    assert_equal([1, 2, 3, 4, 7, 8, 9], array)
+    assert_equal([1, 2, 3, 4, 5, 7, 8, 9], array)
   end
 end
