f2b4601fe3a490751b961ed9e97ae45c8522e6c8
mogongzi <superrenyuan@hotmail.com>
2022-06-05 17:44:24 +0800

    using hash set(hash table) to resolve subset array

--- is_subset.rb
+++ is_subset.rb
@@ -1,4 +1,6 @@
-def is_subset(array_a, array_b)
+require 'set'
+
+def subset_nested_loop?(array_a, array_b)
   array_b.each do |b|
     matched = false
     array_a.each do |a|
@@ -12,15 +14,23 @@ def is_subset(array_a, array_b)
   true
 end
 
+def subset?(array_a, array_b)
+  larger = array_a.to_set
+  array_b.each do |a|
+    return false unless larger.include?(a)
+  end
+  true
+end
+
 require 'test/unit'
 
 class IsSubsetTest < Test::Unit::TestCase
   def test_is_subset
     array_a = %w[a b c d e f]
     array_b = %w[b d f]
-    assert_true(is_subset(array_a, array_b))
+    assert_true(subset?(array_a, array_b))
 
     array_b = %w[b d f h]
-    assert_false(is_subset(array_a, array_b))
+    assert_false(subset?(array_a, array_b))
   end
 end
